7215d86cb3e12bf6d188ce473af2e3655a45ee50
iadgovuser1 <iadgovuser1@users.noreply.github.com>
2017-12-19 18:16:38 -0500

    update telemetry example

--- Examples/TelemetryConnectivity.ps1
+++ Examples/TelemetryConnectivity.ps1
@@ -11,12 +11,45 @@ Function Get-TelemetryConnectivity() {
     [CmdletBinding()]
     [OutputType([System.Collections.Generic.List[pscustomobject]])]
     Param(
+       
+        [Parameter(Mandatory=$false, HelpMessage='Whether to perform a BlueCoat Site Review lookup on the URL. Warning: The BlueCoat Site Review REST API is rate limited.')]
+        [switch]$PerformBluecoatLookup
+    )
+
+    $parameters = $PSBoundParameters
+
+    $isVerbose = $verbosePreference -eq 'Continue'    
+
+    $data = New-Object System.Collections.Generic.List[pscustomobject]
+
+    $data.Add([pscustomobject]@{ Url = 'https://settings-win.data.microsoft.com'; StatusCode = 200; })
+    $data.Add([pscustomobject]@{ Url = 'https://watson.telemetry.microsoft.com'; StatusCode = 200; })
+    $data.Add([pscustomobject]@{ Url = 'https://oca.telemetry.microsoft.com'; StatusCode = 200; })
+    $data.Add([pscustomobject]@{ Url = 'https://vortex.data.microsoft.com/collect/v1'; StatusCode = 404; })
+
+    $results = New-Object System.Collections.Generic.List[pscustomobject]
+
+    $data | ForEach-Object {
+        $targetUrl = $_.Url
+        $statusCode = $_.StatusCode
+
+        $connectivity = Get-Connectivity -Url $_.Url -ExpectedStatusCode $_.StatusCode -PerformBluecoatLookup:$PerformBluecoatLookup -Verbose:$isVerbose
+        $results.Add($connectivity)
+    }  
+
+    return $results
+}
+
+Function Save-TelemetryPConnectivity() {
+    [CmdletBinding()]
+    [OutputType([System.Collections.Generic.List[pscustomobject]])]
+    Param(
+        [Parameter(Mandatory=$false, HelpMessage='Path to save the output to')]
+        [System.Collections.Generic.List[pscustomobject]]$Results,
+
         [Parameter(Mandatory=$false, HelpMessage='Path to save the output to')]
         [string]$OutputPath,
-        
-        [Parameter(Mandatory=$false, HelpMessage='Whether to perform a BlueCoat Site Review lookup on the URL. Warning: The BlueCoat Site Review REST API is rate limited.')]
-        [switch]$PerformBluecoatLookup,
-        
+
         [Parameter(Mandatory=$false, HelpMessage='Compress JSON output')]
         [switch]$Compress
     )
@@ -35,15 +68,8 @@ Function Get-TelemetryConnectivity() {
         New-Item -Path $OutputPath -ItemType Directory
     }
 
-    $targetUrl = [Uri]'https://v10.vortex-win.data.microsoft.com'
-    $connectivity = Get-Connectivity -Url 'https://v10.vortex-win.data.microsoft.com' -ExpectedStatus 404 -PerformBluecoatLookup:$PerformBluecoatLookup -Verbose:$isVerbose
-    
-    $fileName = ($targetUrl.OriginalString.Split([string[]][IO.Path]::GetInvalidFileNameChars(),[StringSplitOptions]::RemoveEmptyEntries)) -join '-'
-    $json = $connectivity | ConvertTo-Json -Depth 5 -Compress:$Compress
+    #$fileName = ($targetUrl.OriginalString.Split([string[]][IO.Path]::GetInvalidFileNameChars(),[StringSplitOptions]::RemoveEmptyEntries)) -join '-'
+    $fileName = 'TelemetryConnectivity'
+    $json = $Results | ConvertTo-Json -Depth 3 -Compress:$Compress
     $json | Out-File -FilePath "$OutputPath\$fileName.json" -NoNewline -Force
-    
-    $connectivity = Get-Connectivity -Url 'https://settings-win.data.microsoft.com' -ExpectedStatus 200 -PerformBluecoatLookup:$PerformBluecoatLookup -Verbose:$isVerbose
-    $connectivity = Get-Connectivity -Url 'https://watson.telemetry.microsoft.com' -ExpectedStatus 200 -PerformBluecoatLookup:$PerformBluecoatLookup -Verbose:$isVerbose
-    $connectivity = Get-Connectivity -Url 'https://oca.telemetry.microsoft.com' -ExpectedStatus 200 -PerformBluecoatLookup:$PerformBluecoatLookup -Verbose:$isVerbose
-    $connectivity = Get-Connectivity -Url 'https://vortex.data.microsoft.com/collect/v1' -ExpectedStatus 404 -PerformBluecoatLookup:$PerformBluecoatLookup -Verbose:$isVerbose
 }
